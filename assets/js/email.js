c0f753ad3eb07130908334f2e1f6a0e2c29677a9
nathantbenke <nathantbenke@gmail.com>
2024-09-28 15:53:01 +0100

    Fix EmailJS

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,15 +4,23 @@ function SendMail() {
         from_name : document.getElementById("fullName").value,
         email_id : document.getElementById("email_id").value,
         subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
+        message : document.getElementById("message").value,
     };
 
-    emailjs.send("service_id3xvvf", "template_p1unncs", params).then(
-        (response) => {
-            console.log("Success! " + res.status);             
-        },
-        (error) => {
-            console.log("Failed... " + error);             
-        }, 
-    ); 
+    const serviceID = "service_id3xvvf"; 
+    const templateID = "template_p1unncs"; 
+
+    emailjs.send(serviceID, templateID, params)
+    .then((res) => {
+    alert("Your message has been sent successfully!");
+    // Clear the text boxes after successful email sending
+    document.getElementById("fullName").value = "";
+    document.getElementById("email_id").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    })
+    .catch((err) => {
+    alert("An error occurred while sending your message. Please try again.");
+    console.error(err);
+    });
 }
